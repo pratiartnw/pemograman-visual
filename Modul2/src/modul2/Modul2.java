834081c2c818d23e6fb644d7d8dfcb0b0a1847c2
pratiartnw <141471498+pratiartnw@users.noreply.github.com>
2024-08-06 15:26:02 +0700

    Add files via upload
    Tugas modul 2 pemograman visual

---
+++ Modul2/src/modul2/Modul2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Modul2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Tugas1 tugas=new Tugas1();
+        tugas.setVisible(true);
+    }
+    
+}
